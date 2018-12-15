556c348ee86a26defb3435468d58b9418493a94d
Asif Sharif <asifsharif@Asifs-MacBook-Pro.local>
2018-12-16 02:41:36 +0500

    picker popup added

--- picker-cascader/picker-cascader.js
+++ picker-cascader/picker-cascader.js
@@ -1,46 +1,86 @@
-import React, {Component} from 'react';
-import {Modal, Text, TouchableHighlight, View, Alert} from 'react-native';
+import React, { Component } from 'react';
+import { Modal, Text, TouchableHighlight, View, Alert,
+   Button, SectionList, StyleSheet, FlatList } from 'react-native';
+import Dialog, { DialogContent } from 'react-native-popup-dialog';
 
-export  class PickerCascader extends Component {
+export class PickerCascader extends Component {
   state = {
     modalVisible: false,
+    visible:false
   };
 
   setModalVisible(visible) {
-    this.setState({modalVisible: visible});
+    this.setState({ modalVisible: visible });
   }
 
+  
+  
+
   render() {
+    
+      
     return (
-      <View style={{marginTop: 22}}>
-        <Modal
-          animationType="slide"
-          transparent={false}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {
-            Alert.alert('Modal has been closed.');
-          }}>
-          <View style={{marginTop: 22}}>
-            <View>
-              <Text>Hello World!</Text>
-
-              <TouchableHighlight
-                onPress={() => {
-                  this.setModalVisible(!this.state.modalVisible);
-                }}>
-                <Text>Hide Modal</Text>
-              </TouchableHighlight>
-            </View>
-          </View>
-        </Modal>
-
-        <TouchableHighlight
+      <View style={{ marginTop: 22 }}>
+        <Button
+          title="Show Dialog"
           onPress={() => {
-            this.setModalVisible(true);
-          }}>
-          <Text>Show Modal1</Text>
-        </TouchableHighlight>
+            this.setState({ visible: true });
+          }}
+        />
+        <Dialog
+        height={0.5}
+          visible={this.state.visible}
+          onTouchOutside={() => {
+            this.setState({ visible: false });
+          }}
+        >
+          <DialogContent>
+            <Text>Custom Content picker</Text>
+            <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+            {/* <SectionList
+          sections={[
+            {title: 'D', data: ['Devin']},
+            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          keyExtractor={(item, index) => index}
+        /> */}
+          </DialogContent>
+        </Dialog>
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
